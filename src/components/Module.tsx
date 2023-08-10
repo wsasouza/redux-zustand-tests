7ce1d968f5c04022cf7c0f048ca9441c124094db
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-08-09 23:40:45 -0300

    Feat: Sinalizando aula ativa

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -14,6 +14,12 @@ interface ModuleProps {
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   const dispatch = useDispatch()
 
+  const { currentLessonIndex, currentModuleIndex } = useAppSelector((state) => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    return { currentLessonIndex, currentModuleIndex }
+  })
+
   const lessons = useAppSelector((state) => {
     return state.player.course.modules[moduleIndex].lessons
   })
@@ -36,12 +42,16 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
           {lessons.map((lesson, lessonIndex) => {
+            const isCurrent =
+              currentModuleIndex === moduleIndex &&
+              currentLessonIndex === lessonIndex
             return (
               <Lesson
                 key={lesson.id}
                 title={lesson.title}
                 duration={lesson.duration}
                 onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                isCurrent={isCurrent}
               />
             )
           })}
