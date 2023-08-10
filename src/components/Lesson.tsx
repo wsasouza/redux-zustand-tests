8a2eea7724acd36e59c6c5b01a7ec8dcf8d01cba
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-08-09 23:22:00 -0300

    Feat: Selecionando video atual

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -3,12 +3,14 @@ import { Video } from 'lucide-react'
 interface LessonProps {
   title: string
   duration: string
+  onPlay: () => void
 }
 
-export function Lesson({ title, duration }: LessonProps) {
+export function Lesson({ title, duration, onPlay }: LessonProps) {
   return (
     <button
       type="button"
+      onClick={onPlay}
       className="flex items-center gap-3 text-sm text-zinc-400"
     >
       <Video className="h-4 w-4 text-zinc-500" />
--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,13 +1,24 @@
 import ReactPlayer from 'react-player/youtube'
+import { useAppSelector } from '../store'
 
 export function VideoPlayer() {
+  const lesson = useAppSelector((state) => {
+    const { currentLessonIndex, currentModuleIndex } = state.player
+
+    const currentLesson =
+      state.player.course.modules[currentModuleIndex].lessons[
+        currentLessonIndex
+      ]
+    return currentLesson
+  })
+
   return (
     <div className="aspect-video w-full bg-zinc-950">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
-        url="https://www.youtube.com/watch?v=_J5QlMq58SQ"
+        url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
   )
